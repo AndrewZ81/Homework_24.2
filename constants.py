00e456c78bc71b6f20f67b1f51a87a2bafa64930
Andrew Zaiko <andrewzaiko@gmail.com>
2022-11-08 11:16:13 +0300

    Добавил ключ "filter_by_regex" в CMDS_TO_EXECUTE

--- constants.py
+++ constants.py
@@ -1,8 +1,9 @@
-from app.functions import filter_query, map_query, unique_query, sort_query, limit_query
+from app.functions import filter_query, map_query, unique_query, sort_query, limit_query, filter_by_regex_query
 
 # Создаём словарь для выбора функции в зависимости от строки команды
 CMDS_TO_EXECUTE = {
     "filter": filter_query,
+    "filter_by_regex": filter_by_regex_query,
     "map": map_query,
     "unique": unique_query,
     "sort": sort_query,
